de50d09cabc087802b3f9d10ece29236b1b10200
GT3CH1 <gavinpease@gmail.com>
2017-02-19 19:49:18 -0700

    Adding some obstacles...

--- js/sketch.js
+++ js/sketch.js
@@ -1,84 +1,205 @@
-(function() {
+(function () {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
 })();
- 
+
 var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
-    width = 500,
+    width = window.innerWidth - 2,
     height = 200,
     player = {
-      x : width/2,
-      y : height - 5,
-      width : 5,
-      height : 5,
-      speed: 3,
-      velX: 0,
-      velY: 0,
-      jumping: false
+        x: width / width + 20,
+        y: height - 15,
+        width: 5,
+        height: 5,
+        speed: 3,
+        velX: 0,
+        velY: 0,
+        jumping: false,
+        grounded: false
     },
     keys = [],
     friction = 0.8,
-    gravity = 0.2;
- 
-canvas.width = width; //get width
-canvas.height = height; //get height
- //update function
-function update(){
-  // check keys
-    if (keys[38] || keys[32]) {
+    gravity = 0.5;
+
+var boxes = [];
+
+// dimensions
+
+//boundaries
+boxes.push({
+    x: 0,
+    y: 0,
+    width: 10,
+    height: height
+});
+boxes.push({
+    x: 0,
+    y: -height - 2,
+    width: width,
+    height: 50
+});
+boxes.push({
+    x: 0,
+    y: height - 2,
+    width: 80,
+    height: 50
+});
+boxes.push({
+    x: width - 10,
+    y: 0,
+    width: 0,
+    height: height
+});
+
+
+
+//obstacles
+boxes.push({
+    x: 70,
+    y: height-30,
+    width: 10,
+    height: 5
+});
+
+boxes.push({
+    x: 40,
+    y: height-50,
+    width: 10,
+    height: 5
+});
+boxes.push({
+    x: 20,
+    y: height-70,
+    width: 10,
+    height: 5
+});
+boxes.push({
+    x: 60,
+    y: height-100,
+    width: 10,
+    height: 5
+});
+
+
+
+
+
+canvas.width = width;
+canvas.height = height;
+
+function update() {
+    // check keys
+    if (keys[38] || keys[32] || keys[87]) {
         // up arrow or space
-      if(!player.jumping){
-       player.jumping = true;
-       player.velY = -player.speed*2;
-      }
+        if (!player.jumping && player.grounded) {
+            player.jumping = true;
+            player.grounded = false;
+            player.velY = -player.speed * 2;
+        }
     }
-    if (keys[39]) {
+    if (keys[39] || keys[68]) {
         // right arrow
-        if (player.velX < player.speed) {             
-            player.velX++;         
-         }     
-    }     
-    if (keys[37]) {         
-        // left arrow         
+        if (player.velX < player.speed) {
+            player.velX++;
+        }
+    }
+    if (keys[37] || keys[65]) {
+        // left arrow
         if (player.velX > -player.speed) {
             player.velX--;
         }
     }
- 
+
     player.velX *= friction;
- 
     player.velY += gravity;
- 
+
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    
+    player.grounded = false;
+    for (var i = 0; i < boxes.length; i++) {
+        ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+        
+        var dir = colCheck(player, boxes[i]);
+
+        if (dir === "l" || dir === "r") {
+            player.velX = 0;
+            player.jumping = false;
+        } else if (dir === "b") {
+            player.grounded = true;
+            player.jumping = false;
+        } else if (dir === "t") {
+            player.velY *= -1;
+        }
+
+    }
+    
+    if(player.grounded){
+         player.velY = 0;
+    }
+    //console.log(player.y);
     player.x += player.velX;
     player.y += player.velY;
- 
-    if (player.x >= width-player.width) {
-        player.x = width-player.width;
-    } else if (player.x <= 0) {         
-        player.x = 0;     
-    }    
-  
-    if(player.y >= height-player.height){
-        player.y = height - player.height;
-        player.jumping = false;
+
+    ctx.fill();
+    ctx.fillStyle = "red";
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    requestAnimationFrame(update);
+    
+    if(player.y >= 200){
+        player.x = width / width + 20
+        player.y =height - 15
+        player.velX = 0;
     }
- 
-  ctx.clearRect(0,0,width,height);
-  ctx.fillStyle = "red";
-  ctx.fillRect(player.x, player.y, player.width, player.height);
- 
-  requestAnimationFrame(update);
 }
- 
-document.body.addEventListener("keydown", function(e) {
+
+function colCheck(shapeA, shapeB) {
+    // get the vectors to check against
+    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+        vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+        // add the half widths and half heights of the objects
+        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+        hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+        colDir = null;
+
+    // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+        // figures out on which side we are colliding (top, bottom, left, or right)
+        var oX = hWidths - Math.abs(vX),
+            oY = hHeights - Math.abs(vY);
+        if (oX >= oY) {
+            if (vY > 0) {
+                colDir = "t";
+                shapeA.y += oY;
+            } else {
+                colDir = "b";
+                shapeA.y -= oY;
+            }
+        } else {
+            if (vX > 0) {
+                colDir = "l";
+                shapeA.x += oX;
+            } else {
+                colDir = "r";
+                shapeA.x -= oX;
+            }
+        }
+    }
+    return colDir;
+}
+
+document.body.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
 });
- 
-document.body.addEventListener("keyup", function(e) {
+
+document.body.addEventListener("keyup", function (e) {
     keys[e.keyCode] = false;
 });
- 
-window.addEventListener("load",function(){
+
+
+window.addEventListener("load", function () {
     update();
 });
